3effce28e7f5d94a257007e6f7385d789a616152
thiago_martins <thiago86.tech@gmail.com>
2025-04-23 22:40:45 -0300

    Primeiro commit

---
+++ empresa/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tecnologias
+
+# Create your views here.
+
+def nova_empresa(request):
+    techs=Tecnologias.objects.all()
+    return render(request,'nova_empresa.html', {'techs':techs})
